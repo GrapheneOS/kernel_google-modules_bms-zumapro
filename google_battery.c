92de35473e28ed99f22d8c4378e70b7d61e48e62
Jack Wu <wjack@google.com>
2022-10-18 12:11:22 +0800

    google_battery: use scratch space in the if-pmic to save shutdown flag
    1. Set shutdown flag when the device shutdown from
       battery critical low in OS mode.
    2. Clear shutdown flag on every boot.
    
    Bug: 251680171
    Signed-off-by: Jack Wu <wjack@google.com>
    Change-Id: I493dff8440e6c70b8e229556b62e2f31e947216b
    (cherry picked from commit 3c3569ce0d27e60391d420edc5a894e6237aad75)

--- google_battery.c
+++ google_battery.c
@@ -548,6 +548,9 @@ struct batt_drv {
 
 	/* battery pack status */
 	struct batt_bpst bpst_state;
+
+	/* shutdown flag */
+	int boot_to_os_attempts;
 };
 
 static int gbatt_get_temp(const struct batt_drv *batt_drv, int *temp);
@@ -7002,6 +7005,9 @@ static int batt_init_debugfs(struct batt_drv *batt_drv)
 			   &batt_drv->health_data.bhi_data.res_state.ravg_soc_high);
 	debugfs_create_file("ravg", 0400, de,  batt_drv, &debug_ravg_fops);
 
+	/* shutdown flag */
+	debugfs_create_u32("boot_to_os_attempts", 0660, de, &batt_drv->boot_to_os_attempts);
+
 	return 0;
 }
 
@@ -7388,6 +7394,38 @@ static int google_battery_init_hist_work(struct batt_drv *batt_drv )
 	return 0;
 }
 
+#define BOOT_TO_OS_ATTEMPTS 3
+
+static int batt_init_shutdown_flag(struct batt_drv *batt_drv)
+{
+	u8 data;
+	int ret;
+
+	ret = gbms_storage_read(GBMS_TAG_SUFG, &data, sizeof(data));
+	if (ret < 0)
+		return -EIO;
+
+	batt_drv->boot_to_os_attempts = data;
+
+	/* reset battery shutdown flag */
+	data = 0;
+	ret = gbms_storage_write(GBMS_TAG_SUFG, &data, sizeof(data));
+
+	return (ret < 0) ? -EIO : 0;
+}
+
+static int batt_set_shutdown_flag(struct batt_drv *batt_drv)
+{
+	u8 data = batt_drv->boot_to_os_attempts;
+	int ret;
+
+	if (data == 0)
+		data = BOOT_TO_OS_ATTEMPTS;
+
+	ret = gbms_storage_write(GBMS_TAG_SUFG, &data, sizeof(data));
+
+	return (ret < 0) ? -EIO : 0;
+}
 
 /*
  * poll the battery, run SOC%, dead battery, critical.
@@ -7475,6 +7513,13 @@ static void google_battery_work(struct work_struct *work)
 			pr_debug("%s: change of capacity level %d->%d\n",
 				 __func__, batt_drv->capacity_level,
 				 level);
+
+			/* set battery critical shutdown */
+			if (level == POWER_SUPPLY_CAPACITY_LEVEL_CRITICAL) {
+				ret = batt_set_shutdown_flag(batt_drv);
+				if (ret < 0)
+					pr_warn("failed to write shutdown flag, ret=%d\n", ret);
+			}
 
 			batt_drv->capacity_level = level;
 			notify_psy_changed = true;
@@ -8344,6 +8389,7 @@ static void google_battery_init_work(struct work_struct *work)
 	batt_drv->hold_taper_ws = false;
 	batt_drv->fake_temp = 0;
 	batt_drv->fake_battery_present = -1;
+	batt_drv->boot_to_os_attempts = 0;
 	batt_reset_chg_drv_state(batt_drv);
 
 	mutex_init(&batt_drv->chg_lock);
@@ -8428,6 +8474,11 @@ static void google_battery_init_work(struct work_struct *work)
 	ret = batt_init_bpst_profile(batt_drv);
 	if (ret < 0)
 		pr_err("bpst profile disabled, ret=%d\n", ret);
+
+	/* init shutdown flag */
+	ret = batt_init_shutdown_flag(batt_drv);
+	if (ret < 0)
+		pr_err("failed to init shutdown flag, ret=%d\n", ret);
 
 	/* cycle count is cached: read here bc SSOC, chg_profile might use it */
 	batt_update_cycle_count(batt_drv);
