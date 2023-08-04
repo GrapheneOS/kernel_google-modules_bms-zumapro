6f95b9da9266e7eaa9f8fee7d7ab89b0220cf11b
YiHsiang Peng <yihsiangpeng@google.com>
2023-08-04 03:09:45 +0000

    p9221_charger: add ICL ramp down for HPP->EPP
    Bug: 293609880
    Change-Id: I6e94a968f536e68fdfefb59b92f15214b7c684fc
    Signed-off-by: YiHsiang Peng <yihsiangpeng@google.com>

--- p9221_chip.c
+++ p9221_chip.c
@@ -2146,6 +2146,23 @@ static int ra9530_prop_mode_enable(struct p9221_charger_data *chgr, int req_pwr)
 
 	msleep(50);
 
+	if (chgr->pdata->has_sw_ramp) {
+		dev_dbg(&chgr->client->dev, "%s: voter=%s, icl=%d\n",
+			__func__, P9221_HPP_VOTER, P9XXX_PROPMODE_ENABLE_ICL_UA);
+		ret = p9xxx_sw_ramp_icl(chgr, P9XXX_PROPMODE_ENABLE_ICL_UA);
+		if (ret < 0)
+			return ret;
+
+		ret = gvotable_cast_int_vote(chgr->dc_icl_votable, P9221_HPP_VOTER,
+					     P9XXX_PROPMODE_ENABLE_ICL_UA, true);
+		if (ret == 0)
+			ret = gvotable_cast_int_vote(chgr->dc_icl_votable,
+						     P9221_RAMP_VOTER, 0, false);
+		if (ret < 0)
+			dev_err(&chgr->client->dev, "%s: cannot setup sw ramp (%d)\n",
+				__func__, ret);
+	}
+
 	if (!chgr->chg_mode_votable)
 		chgr->chg_mode_votable =
 			gvotable_election_get_handle(GBMS_MODE_VOTABLE);
@@ -2268,7 +2285,7 @@ err_exit:
 		int rc;
 
 		rc = gvotable_cast_int_vote(chgr->dc_icl_votable, P9221_HPP_VOTER,
-					    P9XXX_CDMODE_ENABLE_ICL_UA, false);
+					    P9XXX_PROPMODE_ENABLE_ICL_UA, false);
 		if (rc <0)
 			dev_err(&chgr->client->dev, "%s: cannot remove HPP voter (%d)\n",
 				__func__, ret);
