d7201d7bd193ec68cd6da92043491446d602d773
Daniel Okazaki <dtokazaki@google.com>
2023-11-22 21:57:25 +0000

    max77779_i2cm: Splitting probe from i2c bus
    Driver is modular, can support multiple busses
    Split driver into two steps: one for bus specifics, and one common
    
    Bug: 308476363
    Test: probe, peripherals probe on i2cm
    Change-Id: I650b8ae61f0cbe2584ea211f4b2ccbfa1efd6d94
    Signed-off-by: Daniel Okazaki <dtokazaki@google.com>

--- max77779_i2cm.c
+++ max77779_i2cm.c
@@ -13,91 +13,7 @@
 #include <linux/regmap.h>
 
 #include "max77779_pmic.h"
-
-#define DONEI_SET(v) (((v) << MAX77779_I2CM_INTERRUPT_DONEI_SHIFT) & \
-		MAX77779_I2CM_INTERRUPT_DONEI_MASK)
-#define DONEI_GET(v) (((v) & MAX77779_I2CM_INTERRUPT_DONEI_MASK) >> \
-		MAX77779_I2CM_INTERRUPT_DONEI_SHIFT)
-#define ERRI_SET(v) (((v) << MAX77779_I2CM_INTERRUPT_ERRI_SHIFT) & \
-		MAX77779_I2CM_INTERRUPT_ERRI_MASK)
-#define ERRI_GET(v) (((v) & MAX77779_I2CM_INTERRUPT_ERRI_MASK) >> \
-		MAX77779_I2CM_INTERRUPT_ERRI_SHIFT)
-
-#define DONEIM_SET(v) (((v) << MAX77779_I2CM_INTMASK_DONEIM_SHIFT) & \
-		MAX77779_I2CM_INTMASK_DONEIM_MASK)
-#define DONEIM_GET(v) (((v) & MAX77779_I2CM_INTMASK_DONEIM_MASK) >> \
-		MAX77779_I2CM_INTMASK_DONEIM_SHIFT)
-#define ERRIM_SET(v) (((v) << MAX77779_I2CM_INTMASK_ERRIM_SHIFT) & \
-		MAX77779_I2CM_INTMASK_ERRIM_MASK)
-#define ERRIM_GET(v) (((v) & MAX77779_I2CM_INTMASK_ERRIM_MASK) >> \
-		MAX77779_I2CM_INTMASK_ERRIM_SHIFT)
-
-#define ERROR_SET(v) (((v) << MAX77779_I2CM_STATUS_ERROR_SHIFT) & \
-		MAX77779_I2CM_STATUS_ERROR_MASK)
-#define ERROR_GET(v) (((v) & MAX77779_I2CM_STATUS_ERROR_MASK) >> \
-		MAX77779_I2CM_STATUS_ERROR_SHIFT)
-
-#define I2CEN_SET(v) (((v) << MAX77779_I2CM_CONTROL_I2CEN_SHIFT) & \
-		MAX77779_I2CM_CONTROL_I2CEN_MASK)
-#define I2CEN_GET(v) (((v) & MAX77779_I2CM_CONTROL_I2CEN_MASK) >> \
-		MAX77779_I2CM_CONTROL_I2CEN_SHIFT)
-#define CLOCK_SPEED_SET(v) (((v) << MAX77779_I2CM_CONTROL_CLOCK_SPEED_SHIFT) & \
-		MAX77779_I2CM_CONTROL_CLOCK_SPEED_MASK)
-#define CLOCK_SPEED_GET(v) (((v) & MAX77779_I2CM_CONTROL_CLOCK_SPEED_MASK) >> \
-		MAX77779_I2CM_CONTROL_CLOCK_SPEED_SHIFT)
-
-#define SID_SET(v) (((v) << MAX77779_I2CM_SLADD_SLAVE_ID_SHIFT) & \
-		MAX77779_I2CM_SLADD_SLAVE_ID_MASK)
-#define SID_GET(v) (((v) & MAX77779_I2CM_SLADD_SLAVE_ID_MASK) >> \
-		MAX77779_I2CM_SLADD_SLAVE_ID_SHIFT)
-
-#define TXCNT_SET(v) (((v) << MAX77779_I2CM_TXDATA_CNT_TXCNT_SHIFT) & \
-		MAX77779_I2CM_TXDATA_CNT_TXCNT_MASK)
-#define TXCNT_GET(v) (((v) & MAX77779_I2CM_TXDATA_CNT_TXCNT_MASK) >> \
-		MAX77779_I2CM_TXDATA_CNT_TXCNT_SHIFT)
-
-#define I2CMWRITE_SET(v) (((v) << MAX77779_I2CM_CMD_I2CMWRITE_SHIFT) & \
-		MAX77779_I2CM_CMD_I2CMWRITE_MASK)
-#define I2CMWRITE_GET(v) (((v) & MAX77779_I2CM_CMD_I2CMWRITE_MASK) >> \
-		MAX77779_I2CM_CMD_I2CMWRITE_SHIFT)
-
-#define I2CMREAD_SET(v) (((v) << MAX77779_I2CM_CMD_I2CMREAD_SHIFT) & \
-		MAX77779_I2CM_CMD_I2CMREAD_MASK)
-#define I2CMREAD_GET(v) (((v) & MAX77779_I2CM_CMD_I2CMREAD_MASK) >> \
-		MAX77779_I2CM_CMD_I2CMREAD_SHIFT)
-
-#define I2CM_ERR_ARBITRATION_LOSS(status_err)	(!!((status_err) & BIT(0)))
-#define I2CM_ERR_TIMEOUT(status_err)		(!!((status_err) & BIT(1)))
-#define I2CM_ERR_ADDRESS_NACK(status_err)	(!!((status_err) & BIT(2)))
-#define I2CM_ERR_DATA_NACK(status_err)		(!!((status_err) & BIT(3)))
-#define I2CM_ERR_RX_FIFO_NA(status_err)		(!!((status_err) & BIT(4)))
-#define I2CM_ERR_START_OUT_SEQ(status_err)	(!!((status_err) & BIT(5)))
-#define I2CM_ERR_STOP_OUT_SEQ(status_err)	(!!((status_err) & BIT(6)))
-
-#define I2CM_MAX_REGISTER			MAX77779_I2CM_RX_BUFFER_31
-
-#define MAX77779_TIMEOUT_DEFAULT		0xff
-#define MAX77779_MAX_TIMEOUT			0xff
-#define MAX77779_COMPLETION_TIMEOUT_MS_DEFAULT	20
-#define MAX77779_MAX_SPEED			0x03
-#define MAX77779_SPEED_DEFAULT			0x00
-
-#define MAX77779_I2CM_MAX_WRITE \
-		(MAX77779_I2CM_TX_BUFFER_33 - MAX77779_I2CM_TX_BUFFER_0 + 1)
-#define MAX77779_I2CM_MAX_READ \
-		(MAX77779_I2CM_RX_BUFFER_31 - MAX77779_I2CM_RX_BUFFER_0 + 1)
-
-struct max77779_i2cm_info {
-	struct i2c_adapter	adap;  /* bus */
-	struct i2c_client	*client;
-	struct device		*dev;
-	struct regmap		*regmap;
-	struct completion	xfer_done;
-	unsigned int		timeout;
-	unsigned int		completion_timeout_ms;
-	unsigned int		speed;
-	u8			reg_vals[I2CM_MAX_REGISTER + 1];
-};
+#include "max77779_i2cm.h"
 
 static int max77779_i2cm_done(struct max77779_i2cm_info *info,
 		unsigned int *status)
@@ -298,14 +214,6 @@ static const struct i2c_algorithm max77779_i2cm_algorithm = {
 	.functionality		= max77779_i2cm_func,
 };
 
-static const struct regmap_config max77779_i2cm_regmap_cfg = {
-	.name = "max77779_i2cm_regmap_cfg",
-	.reg_bits = 8,
-	.val_bits = 8,
-	.val_format_endian = REGMAP_ENDIAN_NATIVE,
-	.max_register = I2CM_MAX_REGISTER,
-};
-
 static const struct i2c_adapter_quirks max77779_i2cm_quirks = {
 	.flags = I2C_AQ_COMB_WRITE_THEN_READ |
 		 I2C_AQ_NO_ZERO_LEN |
@@ -317,22 +225,13 @@ static const struct i2c_adapter_quirks max77779_i2cm_quirks = {
 	.max_comb_2nd_msg_len = MAX77779_I2CM_MAX_READ
 };
 
-static int max77779_i2cm_probe(struct i2c_client *client,
-		const struct i2c_device_id *id)
+int max77779_i2cm_init(struct max77779_i2cm_info *info)
 {
-	struct max77779_i2cm_info *info = NULL;
-	struct device *dev = &client->dev;
+	struct device *dev = info->dev;
 	int err = 0;
-
-	if (client->irq < 0)
-		return -EPROBE_DEFER;
 
 	if (!IS_ENABLED(CONFIG_OF))
 		return -EINVAL;
-
-	info = devm_kzalloc(dev, sizeof(*info), GFP_KERNEL);
-	if (!info)
-		return -ENOMEM;
 
 	/* Device Tree Setup */
 	err = of_property_read_u32(dev->of_node, "max77779,timeout",
@@ -356,19 +255,10 @@ static int max77779_i2cm_probe(struct i2c_client *client,
 		info->completion_timeout_ms =
 			MAX77779_COMPLETION_TIMEOUT_MS_DEFAULT;
 
-	/* setup data structures */
-	info->client = client;
-	info->dev = dev;
-	i2c_set_clientdata(client, info);
-	info->regmap = devm_regmap_init_i2c(client, &max77779_i2cm_regmap_cfg);
-	if (IS_ERR(info->regmap)) {
-		dev_err(dev, "Failed to initialize regmap.\n");
-		return -EINVAL;
-	}
 	init_completion(&info->xfer_done);
 
-	if (client->irq) {
-		err = devm_request_threaded_irq(info->dev, client->irq, NULL,
+	if (info->irq) {
+		err = devm_request_threaded_irq(info->dev, info->irq, NULL,
 				max777x9_i2cm_irq,
 				IRQF_TRIGGER_LOW | IRQF_SHARED | IRQF_ONESHOT,
 				"max777x9_i2cm", info);
@@ -406,52 +296,13 @@ static int max77779_i2cm_probe(struct i2c_client *client,
 
 	return err;
 }
+EXPORT_SYMBOL_GPL(max77779_i2cm_init);
 
-static void max77779_i2cm_remove(struct i2c_client *client)
+void max77779_i2cm_remove(struct max77779_i2cm_info *info)
 {
-	struct max77779_i2cm_info *info = dev_get_drvdata(&(client->dev));
-
 	devm_kfree(info->dev, info);
 }
-
-static const struct i2c_device_id id[] = {
-	{ "max77779_i2cm", 0 },
-	{ }
-};
-
-MODULE_DEVICE_TABLE(i2c, id);
-
-#if IS_ENABLED(CONFIG_OF)
-static const struct of_device_id max77779_i2cm_match_table[] = {
-	{ .compatible = "max77779_i2cm",},
-	{ },
-};
-#endif
-
-static struct i2c_driver driver = {
-	.probe		= max77779_i2cm_probe,
-	.remove		= max77779_i2cm_remove,
-	.id_table	= id,
-	.driver = {
-		.name   = "max77779_i2cm",
-#if IS_ENABLED(CONFIG_OF)
-		.of_match_table = max77779_i2cm_match_table,
-#endif
-	},
-};
-
-static int __init max77779_i2cm_init(void)
-{
-	return i2c_add_driver(&driver);
-}
-
-static void __exit max77779_i2cm_exit(void)
-{
-	i2c_del_driver(&driver);
-}
-
-late_initcall(max77779_i2cm_init);
-module_exit(max77779_i2cm_exit);
+EXPORT_SYMBOL_GPL(max77779_i2cm_remove);
 
 MODULE_DESCRIPTION("Maxim 77779 I2C Bridge Driver");
 MODULE_AUTHOR("Jim Wylder <jwylder@google.com>");
